000a00bca92893b7165db7796ae1c7651d5d166a
YOUR NAME <gzheng3@stuy.edu>
2017-08-06 18:31:25 -0400

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,20 @@
+
 class ArticlesController < ApplicationController
-  
+ 
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
   def index
     @articles = Article.all
   end
-
+ 
   def show
     @article = Article.find(params[:id])
   end
-
-
+ 
   def new
     @article = Article.new
   end
-
+ 
   def edit
     @article = Article.find(params[:id])
   end
@@ -26,7 +28,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-
+ 
   def update
     @article = Article.find(params[:id])
  
@@ -36,10 +38,16 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+ 
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-
 end
